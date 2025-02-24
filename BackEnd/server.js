1d1574597e83ebddf34bcbdd522299ca6fc9f131
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-02-24 12:19:01 +0000

    added cors

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const port = 4000; 
 
+//Add CORS Middleware
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //Only run on 4000 port when running
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
