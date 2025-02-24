72b17c7d6b671d72033bc0e32cabff7a3257e83e
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-02-24 11:52:53 +0000

    added routing for navigation bar to 3 pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//Imports for desired navigation pages
 import './App.css';
+import Navigation from './components/Navigation';
+import HomePage from './components/HomePage';
+import CustomQuiz from './components/CustomQuiz';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Routes>
+          {/*Different visitable routes*/}
+          <Route path = "/" element = {<HomePage />} />
+          <Route path = "/playerstats" element = {<PlayerStats />} />
+          <Route path = "/customquiz" element = {<CustomQuiz />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
