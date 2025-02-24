3613de8d7c7e8faeffe7f79b8746758ee41375ea
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-02-24 11:45:47 +0000

    comments

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
+//Imports necessary for navigation bar
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 
 const Navigation = () => 
 {
