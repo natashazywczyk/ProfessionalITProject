3cb0f2cfe126f6be48b446ccc80ebf41625c70ca
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-03-19 15:35:40 +0000

    created fetch function for api questions

--- src/components/GeneralKnowledge.js
+++ src/components/GeneralKnowledge.js
@@ -1,10 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 const GeneralKnowledge = () => {
-    return (
-        <div>
-        {/* Insert GK quiz content, providing questions and multiple choice answers*/}
-            <h1>General Knowledge</h1>
-        </div>
-    );
-}  
+  const [apiData, setApiData] = useState([]); // Store data from API
+  const [loading, setLoading] = useState(true); // Display while fetch is happening
+  const [error, setError] = useState(null); // Handle errors
+
+  useEffect(() => {
+    const fetchData = () => {
+        //Fetch randomised questions from the trivia API
+        axios.get("https://the-trivia-api.com/v2/questions/")
+
+        .then((response) => {
+          console.log(response.data); 
+          setApiData(response.data);    // Store data to useState
+          setLoading(false);    // Set loading to false, as data is loaded
+        })
+
+        .catch((error) => {
+          setError("There was an error with the request.");
+          setLoading(false);           
+          console.error(error); // Display error to console
+        });
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading, please wait...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>; // If there's an error, display error message
+  }
+
+  return (
+    <div>
+      <h1>Welcome to the Trivia App!</h1>
+    </div>
+  );
+};
 
 export default GeneralKnowledge;
