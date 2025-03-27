1fdf319d643a3ee3bdb0a8de0732e8c8594c7ea1
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-03-27 14:27:45 +0000

    general texted explaining the application

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,16 @@ const HomePage = () => {
     return (
         <div>
         {/* Insert Home Page content, providing intro to app and allow users to log in*/}
-            <h1>Welcome</h1>
+            <h1>Welcome!</h1>
+
+            <p>This is your Trivia Quiz app!
+                <br></br><br></br>
+                Here you can take many types of quizzes: General Knowledge, 
+                History, Geography, and even your own custom quizzes! 
+                <br></br>
+                You can also create a profile to keep track of your scores, and compare them to your 
+                friends!
+            </p>
         </div>
     );
 }  
