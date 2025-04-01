b7ab81ffff74b7a89d8b85f1d80fc7cb47acf835
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-04-01 16:21:32 +0100

    include login page routing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PlayerStats from './components/PlayerStats';
 import GeneralKnowledge from './components/GeneralKnowledge';
 import CreateProfile from './components/CreateProfile';
 import Settings from './components/Settings';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path = "/playerstats" element = {<PlayerStats />} />
           <Route path = "/customquiz" element = {<CustomQuiz />} />
           <Route path = "/createprofile" element = {<CreateProfile />} />
+          <Route path = "/login" element = {<LoginPage />} />
           <Route path = "/settings" element = {<Settings />} />
           </Routes>
       </Router> 
