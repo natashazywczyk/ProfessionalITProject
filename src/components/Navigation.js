2f13c4de2961d6ba88973322c19ebd24a76d432d
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-04-24 20:33:57 +0100

    changed create profile to profile settings

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = () =>
                 <Navbar.Brand href="/">Trivia Quiz</Navbar.Brand>
                 <Nav className="me-auto">
                     {/*Different links*/}
-                    <Nav.Link href="/createprofile">Create Profile</Nav.Link>
+                    <Nav.Link href="/createprofile">Profile Settings</Nav.Link>
                     <Nav.Link href="/pickquiz">Pick a Quiz</Nav.Link>
                     <Nav.Link href="/playerstats">Player Statstics</Nav.Link>
                 </Nav>
